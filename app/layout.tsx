504f5e01fc669fda036037ee8141a3e174319c23
gmitsolution3 <gmitsolution3@gmail.com>
2026-08-10 17:45:24 +0600

    feat: routes grouped togather

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-
 const jakarta = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
   subsets: ["latin"],
@@ -24,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={jakarta.variable}>
-      <Header />
       <body className="min-h-screen antialiased">{children}</body>
-      <Footer />
     </html>
   );
 }
