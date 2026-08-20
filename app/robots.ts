960d053d2dbd9ba0b53928eb0e6e727aeb45fba9
gmitsolution3 <gmitsolution3@gmail.com>
2026-08-20 13:56:40 +0600

    updated robot

---
+++ app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://www.gmgroupbd.com";
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/dashboard/",
+        "/api/",
+      ],
+    },
+
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
